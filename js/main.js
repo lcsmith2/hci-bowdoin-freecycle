da124cf34a2654f93dd24d63bf5e9c0017b01005
Laura Yang <yangnuoya@gmail.com>
2023-03-27 18:37:52 -0400

    cancel action

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-
+$(document).ready(function() {
+    // Add click event listener to all "Cancel" buttons
+    $('.btn-primary').click(function() {
+      // Find the parent card of the clicked button
+      var card = $(this).closest('.card');
+  
+      // Remove the parent card from the page
+      card.remove();
+    });
+  });
 
