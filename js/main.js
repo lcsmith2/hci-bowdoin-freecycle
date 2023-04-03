e10149c9edbc710bde2f12f3530c42ae3b9620a4
lcsmith2 <lsmith2@bowdoin.edu>
2023-04-03 13:39:21 -0400

    Add dashboard and login/logout links to navbar

--- js/main.js
+++ js/main.js
@@ -15,3 +15,33 @@ function getListingsData() {
   }
   return JSON.parse(localStorage.listingsData);
 }
+function userLoggedIn() {
+  return localStorage.user !== undefined && localStorage.user !== null;
+}
+
+function updateNav() {
+  var navbar = document.getElementsByClassName("navbar-nav")[0];
+  var loginLogout = document.createElement("li");
+  var loginLogoutLink = document.createElement("a");
+  if (!userLoggedIn()) {
+    loginLogoutLink.href = "login.html";
+    loginLogoutLink.appendChild(document.createTextNode("Log in"));
+  }
+  else {
+    var dashboard = document.createElement("li");
+    var dashboardLink = document.createElement("a");
+    dashboardLink.href = "dashboard.html";
+    dashboardLink.appendChild(document.createTextNode("Dashboard"));
+    dashboard.appendChild(dashboardLink);
+    navbar.appendChild(dashboard);
+    loginLogoutLink.href = "logged-out.html";
+    loginLogoutLink.appendChild(document.createTextNode("Log out"));
+  }
+  loginLogout.appendChild(loginLogoutLink);
+  navbar.appendChild(loginLogout);
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    updateNav();
+});
