4f8c074c07bdd938c66a04443839dfa0e4e013e7
lcsmith2 <lsmith2@bowdoin.edu>
2023-04-05 13:04:50 -0400

    Fix listings.js

--- js/listings.js
+++ js/listings.js
@@ -304,21 +304,3 @@ function displayFilterListings(terms) {
 
 
 }
-
-
-function displayListings() {
-
-    var listingsData = getListingsData();
-
-    var listingsDiv = document.getElementsByClassName("listings")[0];
-    listingsDiv.replaceChildren();
-    listingsData.forEach(function(listingData) {
-        listingsDiv.appendChild(getListing(listingData, true))
-    });
-}
-  
-
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    displayListings();
-});
