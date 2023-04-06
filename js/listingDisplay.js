412998f48e0f259af24128132f4eb849e7f5274e
lcsmith2 <lsmith2@bowdoin.edu>
2023-04-05 21:49:27 -0400

    Fix filtering categories from home page

--- js/listingDisplay.js
+++ js/listingDisplay.js
@@ -222,15 +222,6 @@ function displayCategoryFilterListings(category){
   
   }
 
-function displayListings() {
-    var listingsData = getListingsData();
-
-    var listingsDiv = document.getElementsByClassName("listings")[0];
-    listingsDiv.replaceChildren();
-    listingsData.forEach(function(listingData) {
-        listingsDiv.appendChild(getListing(listingData, true))
-    });
-}
   
 document.addEventListener("DOMContentLoaded", function(event) {
     displayListings();
