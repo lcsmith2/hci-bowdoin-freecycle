04870c9f285693b0d1ff3d4e61ef487e41a2e1c8
lcsmith2 <lsmith2@bowdoin.edu>
2023-04-05 21:15:02 -0400

    Populate request confirmed page

--- js/listingDisplay.js
+++ js/listingDisplay.js
@@ -215,6 +215,16 @@ function displayCategoryFilterListings(category){
   
   }
 
+function displayListings() {
+    var listingsData = getListingsData();
+
+    var listingsDiv = document.getElementsByClassName("listings")[0];
+    listingsDiv.replaceChildren();
+    listingsData.forEach(function(listingData) {
+        listingsDiv.appendChild(getListing(listingData, true))
+    });
+}
+  
 document.addEventListener("DOMContentLoaded", function(event) {
     displayListings();
 });
