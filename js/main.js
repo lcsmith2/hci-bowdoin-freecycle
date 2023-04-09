fbfdaf00ed21461b37008e53b4aaf329bec62141
Laura Yang <yangnuoya@gmail.com>
2023-04-08 20:11:33 -0400

    update dashboard

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-
-
-// $(document).ready(function() {
-//     // Add click event listener to all "Cancel" buttons
-//     $('.btn-primary').click(function() {
-//       // Find the parent card of the clicked button
-//       var card = $(this).closest('.card');
-  
-//       // Remove the parent card from the page
-//       card.remove();
-//     });
-//   });
-
 function getListingsData() {
   if (localStorage.listingsData === undefined) {
     localStorage.listingsData = JSON.stringify(defaultListingsData);
