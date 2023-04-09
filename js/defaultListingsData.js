e86acb298903095296754c6a4139936f5ba11566
Abby Mueller <68559641+amueller8@users.noreply.github.com>
2023-04-09 16:36:07 -0400

    populate listings

--- js/defaultListingsData.js
+++ js/defaultListingsData.js
@@ -58,6 +58,158 @@ var defaultListingsData = [
                 user: "user",
             }
         ],
+    },
+    {
+        id: "4",
+        user: "user4",
+        name: "Keurig Coffee Machine",
+        image: "./assets/keurig.jpg",
+        amount: "2",
+        category: "kitchen",
+        condition: "Like new",
+        price: "10.00",
+        description: "Cute keurig coffee machine. Needs K-cups. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        requests: [
+            {
+                user: "user1",
+            }
+        ],
+    },
+    {
+        id: "5",
+        user: "user2",
+        name: "Area Rug",
+        image: "./assets/rug.jpg",
+        amount: "2",
+        category: "dorm",
+        condition: "Good",
+        price: "Free",
+        description: "Large 5x7 rug, black with white detailing with no stains or damage.",
+        requests: [
+            {
+                user: "user1",
+            }
+        ],
+    },
+    {
+        id: "6",
+        user: "user3",
+        name: "Velvet Hangers",
+        image: "./assets/hangers.jpeg",
+        amount: "1",
+        category: "other",
+        condition: "Like new",
+        price: "2.00",
+        description: "10-pack of velvety hangers, unopened, from target. ",
+        requests: [
+        ],
+    },
+
+    {
+        id: "7",
+        user: "user3",
+        name: "Swiffer",
+        image: "./assets/swiffer.jpg",
+        amount: "3",
+        category: "other",
+        condition: "Fair",
+        price: "Free",
+        description: "Swiffer in decent condition, can be used for wet or dry swiffing. Needs Swiffer sheets.",
+        requests: [
+        ],
+    },
+    {
+        id: "8",
+        user: "user2",
+        name: "Ironing Board",
+        image: "./assets/ironboard.jpg",
+        amount: "3",
+        category: "other",
+        condition: "Good",
+        price: "Free",
+        description: "Folding ironing board, bought new last year and just one year old. does not come with an iron.",
+        requests: [
+        ],
+    },
+    {
+        id: "9",
+        user: "user3",
+        name: "Laundry Hamper",
+        image: "./assets/hamper.jpg",
+        amount: "1",
+        category: "dorm",
+        condition: "Like new",
+        price: "Free",
+        description: "Giving away because I prefer mesh laundry bag. Laundry hamper from walmart. ",
+        requests: [
+        ],
+    },
+    {
+        id: "10",
+        user: "user1",
+        name: "Shower Caddy",
+        image: "./assets/showercaddy.jpeg",
+        amount: "10",
+        category: "other",
+        condition: "Fair",
+        price: "Free",
+        description: "Assorted plastic shower caddies.",
+        requests: [
+        ],
+    },
+    {
+        id: "11",
+        user: "user1",
+        name: "Folding camping chair ",
+        image: "./assets/campchair.jpg",
+        amount: "6",
+        category: "furniture",
+        condition: "Like new",
+        price: "Free",
+        description: "Assorted folding camping chairs, colors can be selected when you pick up item.",
+        requests: [
+        ],
+    },
+    {
+        id: "12",
+        user: "user2",
+        name: "Yogibo",
+        image: "./assets/yogibo.jpg",
+        amount: "1",
+        category: "furniture",
+        condition: "Good",
+        price: "50.00",
+        description: "One yogibo beanbag pillow chair thing. In decent condition but I'm graduating and can't take it with me.",
+        requests: [
+        ],
+    },
+    {
+        id: "13",
+        user: "user4",
+        name: "Graphing calculator",
+        image: "./assets/graphingcalc.jpg",
+        amount: "1",
+        category: "school",
+        condition: "Like new",
+        price: "30.00",
+        description: "TI-84 CE, mint color. 3 years old.",
+        requests: [
+        ],
+    },
+    {
+        id: "14",
+        user: "user3",
+        name: "Cutlery set",
+        image: "./assets/cutlery.jpg",
+        amount: "1",
+        category: "kitchen",
+        condition: "Fair",
+        price: "Free",
+        description: "Forks, knives, and spoons, assorted. Ikea.",
+        requests: [
+        ],
     }
+
+
 
 ];
